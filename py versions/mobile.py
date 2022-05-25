fbeddd8c35ec1d3fb2425858b5ba170fd23e7382
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2022-05-25 21:46:36 +0200

    Update mobile.py

--- py versions/mobile.py
+++ py versions/mobile.py
@@ -63,12 +63,12 @@ def message_p(msg,color):
 direction='none'   #variable used for tactile controls
 
 def gameLoop():  # creating a function
-    game_over = False  #seting initial state of 
+    game_over = False      #when the game is in progress, the game is not stopped or closed
     game_close = False
     while not game_over:   #main gameover loop
-        game_close = False
-        game_pause = False
-        x1 = dis_width / 2   #setting initial snake position
+        game_close = False    #when the game is in progress, the game is not stopped or closed
+        game_pause = False    
+        x1 = dis_width / 2   #when the game is in progress, the game is not stopped or closed
         y1 = dis_height / 2
         snake_List=[]        #list with all snake positions in screen
         Lenght_of_snake=1    #initial length of the snake
@@ -83,15 +83,16 @@ def gameLoop():  # creating a function
         #pause_bt=pygame.draw.rect(display,red,[650,400,80,80])
         #pygame.display.update()
 
-        x1_change = 0
+        x1_change = 0        #snake movement variables
         y1_change = 0
 
+        #The coordinates of the food that appear randomly in the game area
         foodx = round(random.randrange(0, dis_width - snake_block)/ml)*ml
         foody = round(random.randrange(0, 1450 - snake_block)/ml)*ml
         foodx2 = round(random.randrange(0, dis_width - snake_block)/ml)*ml
         foody2 = round(random.randrange(0, 1450 - snake_block)/ml)*ml
         while not game_over:
-            while game_close==True:
+            while game_close==True: #activation of the loop when the player loses
                 display.fill(black)
                 message("You Lost! Press Q-Quit or C-Play Again", red,320,360)
                 Your_score(maz)
@@ -99,10 +100,10 @@ def gameLoop():  # creating a function
 
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_q:
+                        if event.key == pygame.K_q:      #if the player press "q" key, the game is closed
                             game_over = True
                             game_close = False
-                        if event.key == pygame.K_c:
+                        if event.key == pygame.K_c:      #if the player press "c"key, the game loop in lauched and anothe game begins.
                             gameLoop()
 
             if game_close == True:
@@ -111,67 +112,64 @@ def gameLoop():  # creating a function
             for event in pygame.event.get():              # keys to keyboard
                 if event.type==QUIT:
                         game_over==True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_s:
-                       x1_change=0
-                       y1_change=(snake_block-sre)
-                    elif event.key == pygame.K_w:
+                if event.type == pygame.KEYDOWN:      
+                    if event.key == pygame.K_s:    #if the key s is pressed, the snake goes down
+                        x1_change=0
+                        y1_change=(snake_block-sre)
+                    elif event.key == pygame.K_w:     #if the key w is pressed, the snake goes up
                         x1_change=0
                         y1_change=-(snake_block-sre)
-                    elif event.key == pygame.K_d:
+                    elif event.key == pygame.K_d:     #if the key d is pressed, the snake goes right
                         x1_change=(snake_block-sre)
                         y1_change=0
-                    elif event.key == pygame.K_a:
+                    elif event.key == pygame.K_a:     #if the keyleft is pressed, the snake goes left
                         x1_change=-(snake_block-sre)
                         y1_change=0
-
-            while game_pause == True:
-                clock.tick(0)
+                    elif event.key == pygame.K_p: #if the key "p" is pressed, the game is paused.
+                        game_pause = True
+            
+            while game_pause == True:       #pause loop activation
+                clock.tick(0)          #stops the snake movement in order to keep the snake in the same conditions as before the pause
                 display.fill(black)
-                pause_bt=pygame.draw.rect(display,green,[660,600,30,30])
-                quit_bt=pygame.draw.rect(display,red,[600,600,30,30])
-                message_p("You paused the game// press green to continue or red to quit", red)
+                message_p("You paused the game// press green to continue or red to quit", red)    #pause message display
                 Your_score(maz)
                 pygame.display.update()
 
+               for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_p: #if the player press "p" key a counter of 3 secpond is launched to have time to get ready
+                            display.fill(black)
+                            message_p("3", red)
+                            pygame.display.update()
+                            pygame.time.wait(1000)
+                            display.fill(black)
+                            message_p("2", red)
+                            pygame.display.update()
+                            pygame.time.wait(1000)
+                            display.fill(black)
+                            message_p("1", red)
+                            pygame.display.update()
+                            pygame.time.wait(1000)
+                            game_pause = False
+                    elif event.key == pygame.K_q:     #but if the player press "q" key, the game is closed
+                        pygame.quit()
 
-                if pause_bt.collidepoint(mouse_pos):
-                            direction = 'pause'
-                if quit_bt.collidepoint(mouse_pos):
-                            direction = 'quit'
+            if x1 >= dis_width or x1 < 0 or y1 >= 1450 or y1 < 0:   #compares the position of the head of the snake with the limits of the game
+                game_close = True  #when the track limits are exeed, the game is done      
 
-                if direction == 'pause':
-                    display.fill(black)
-                    message_p("3", red)
-                    pygame.display.update()
-                    pygame.time.wait(1000)
-                    display.fill(black)
-                    message_p("2", red)
-                    pygame.display.update()
-                    pygame.time.wait(1000)
-                    display.fill(black)
-                    message_p("1", red)
-                    pygame.display.update()
-                    pygame.time.wait(1000)
-                    game_pause = False
-                if direction == 'quit':
-                    pygame.quit()
-
-            if x1 >= dis_width or x1 < 0 or y1 >= 1450 or y1 < 0:
-                game_close = True
-
-            x1+=x1_change
+            x1+=x1_change     #updates the new position of the head by adding the movement variable to the position of head. 
             y1+=y1_change
             display.fill(black)
-
+            
+            #food and snake display which were not graphic elements
             pygame.draw.rect(display, red, [foodx, foody, snake_block, snake_block])
             pygame.draw.rect(display, green, [x1, y1, snake_block, snake_block])
             pygame.draw.rect(display, violet, [foodx2, foody2, snake_block, snake_block])
             snake_Head=[]
-            snake_Head.append(x1)
+            snake_Head.append(x1)   #new position of the head
             snake_Head.append(y1)
-            snake_List.append(snake_Head)
-            if len(snake_List)>Lenght_of_snake:
+            snake_List.append(snake_Head)    #update of the snake list
+            if len(snake_List)>Lenght_of_snake:   #used to solve a bug
                 del snake_List[0]
 
             for x in snake_List[:-1]:
